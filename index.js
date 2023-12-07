7bb2640e9a3b2194d8464fd324fc59572d032a03
Markus <markustpeterson@gmail.com>
2023-12-07 16:29:39 -0500

    aa

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var app = require('./config/express');
 
-app.get('/create', (req, res) => {
-	let sql = 'CREATE DATABASE userdb'
-	
-})
-
 var port = process.env.PORT || 3333
 app.listen(port, function(){
 	console.log('Server ON in Port' + port)
